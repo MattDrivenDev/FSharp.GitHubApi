d6f089ee19c3079ae1779651fc4da86c048733d6
saxonmatt <saxonmatt@gmail.com>
2013-01-02 13:56:26 +0000

    Added first implementation of getting a specific (non authenticated) user.

---
+++ src/FSharp.GitHubApi.Tests/Users.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharp.GitHubApi.Users
+
+    open NUnit.Framework
+
+    [<TestFixture>]
+    [<Category("FirstPass")>]
+    type Users() =
+
+        [<Test>]
+        member this.``should be able to get a specified user``() =
+            let getQuery = Users.SpecificUser("saxonmatt")
+            let user = 
+                TestHelper.AuthenticatedUser
+                |> TestHelper.DefaultState
+                |> Users.Get getQuery
+            printfn "%s" user.ErrorMessage
+            Assert.AreEqual(200, user.StatusCode)
+            Assert.IsTrue(user.Content.Login.Equals("saxonmatt"))
+            Assert.IsTrue(user.Content.Name.Equals("Matt Ball"))
+            Assert.IsTrue(user.Content.Blog.Contains("www.saxonmatt.co.uk"))
---
+++ src/FSharp.GitHubApi/Users.fs
@@ -0,0 +1,44 @@
+﻿module Users
+
+    open GitHub
+    open System.Runtime.Serialization
+
+    [<DataContract>]
+    type UserDetails = {
+        [<field: DataMember(Name="login")>]
+        Login: string
+        [<field: DataMember(Name="name")>]
+        Name: string
+        [<field: DataMember(Name="blog")>]
+        Blog: string
+    }
+
+    let internal defaultUserDetails = {
+        Login = "octocat"
+        Name = "monalisa octocat"
+        Blog = "https://github.com/blog"
+    }
+
+    type GetParameters = 
+        | AuthenticatedUser
+        | SpecificUser of string
+
+    let internal getSpecificUser username state = 
+        match RestHelper.Get { Resource = (sprintf "users/%s" username) } state with
+        | RestHelper.Success(json) ->
+            let userDetails = json |> JsonHelper.DeserializeJson<UserDetails>
+            match userDetails with
+            | Some(ud) -> { StatusCode = 200; Content = ud; ErrorMessage = "" }
+            | None -> { StatusCode = 200; Content = defaultUserDetails; ErrorMessage = "Cannot deserialize User details, returned api default instead" }
+        | RestHelper.Failed(reason) -> { StatusCode = 0; Content = defaultUserDetails; ErrorMessage = reason }
+
+    let Get query state = 
+        match query with
+        | AuthenticatedUser ->
+            { 
+                StatusCode = 0
+                ErrorMessage = "Not implemented yet"
+                Content = { Login = ""; Name = ""; Blog = ""; }
+            }   
+        | SpecificUser(u) ->
+            getSpecificUser u state
